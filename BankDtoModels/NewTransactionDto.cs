5f19deb417634e70449e0f065d4f1771d35a8427
Ivan Zhenin <ofdof44488kkofsdkofs@mail.ru>
2024-11-15 13:29:18 +0500

    Add new transactiondto, update transactiondto

---
+++ BankDtoModels/NewTransactionDto.cs
@@ -0,0 +1,21 @@
+﻿using BankDtoModels.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BankDtoModels
+{
+	public class NewTransactionDto : IDto
+	{
+		[Required(ErrorMessage = "Поле обязательно для заполнения")]
+		[DisplayName("Номер отправителя")]
+		public uint SenderId { get; set; }
+
+		[Required(ErrorMessage = "Поле обязательно для заполнения")]
+		[DisplayName("Номер получателя")]
+		public uint RecipientId { get; set; }
+
+		[Required(ErrorMessage = "Поле обязательно для заполнения")]
+		[DisplayName("Сумма транзакции")]
+		public decimal TransactionAmount { get; set; }
+	}
+}
--- BankDtoModels/TransactionDto.cs
+++ BankDtoModels/TransactionDto.cs
@@ -4,23 +4,11 @@ using BankDtoModels.Interfaces;
 
 namespace BankDtoModels
 {
-	public class TransactionDto : IDto
+	public class TransactionDto : NewTransactionDto
 	{
 		[Required(ErrorMessage = "Поле обязательно для заполнения")]
 		[DisplayName("Номер транзакции")]
 		public Guid Id { get; set; }
-
-		[Required(ErrorMessage = "Поле обязательно для заполнения")]
-		[DisplayName("Номер отправителя")]
-		public uint SenderId { get; set; }
-
-		[Required(ErrorMessage = "Поле обязательно для заполнения")]
-		[DisplayName("Номер получателя")]
-		public uint RecipientId { get; set; }
-
-		[Required(ErrorMessage = "Поле обязательно для заполнения")]
-		[DisplayName("Сумма транзакции")]
-		public decimal TransactionAmount { get; set; }
 
 		[Required(ErrorMessage = "Поле обязательно для заполнения")]
 		[DisplayName("Дата создания транзакции")]
